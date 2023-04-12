4ae2d245866cc89608384be25aefff04d1f81fc5
Zafar <baigzafar@gmail.com>
2023-04-12 12:18:48 +0200

    first commit

---
+++ jss/script.js
@@ -0,0 +1,29 @@
+const form = document.querySelector("form");
+const email = document.getElementById("mail");
+const emailError = document.querySelector("#mail + span.error");
+
+email.addEventListener("input", (event) => {
+  if (email.validity.valid) {
+    emailError.textContent = ""; // Reset the content of the message
+    emailError.className = "error"; // Reset the visual state of the message
+  } else {
+    showError();
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  if (!email.validity.valid) {
+    showError();
+    event.preventDefault();
+  }
+});
+
+function showError() {
+  if (email.validity.valueMissing) {
+    emailError.textContent = "You need to enter an email address.";
+  } else if (email.validity.typeMismatch) {
+    emailError.textContent = "Please enter a valid email address.";
+  }
+
+  emailError.className = "error active";
+}
